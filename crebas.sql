d16e62fd19c763ce46df1eb2adef7d763a7a2811
ludovic <ludo@mail.com>
2011-11-02 17:08:11 +0100

    af

--- crebas.sql
+++ crebas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.2deb1
+-- version 3.3.7deb5build0.10.10.1
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Mer 26 Octobre 2011 à 11:26
--- Version du serveur: 5.1.41
--- Version de PHP: 5.3.2-1ubuntu4.10
+-- Généré le : Mer 02 Novembre 2011 à 17:07
+-- Version du serveur: 5.1.49
+-- Version de PHP: 5.3.3-1ubuntu9.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -45,7 +45,6 @@ CREATE TABLE IF NOT EXISTS `contact` (
 --
 
 INSERT INTO `contact` (`idcontact`, `identreprise`, `prenomcontact`, `nomcontact`, `fonctioncontact`, `dateajout`, `datederniereactivite`, `telfixecontact`, `telmobilecontact`, `mailcontact`) VALUES
-(0, 2, 'Nicolas', 'Dupond', 'Développeur', '2011-10-26', '2011-10-26', '1928393', '9283938', 'nico@mail.de'),
 (1, 2, 'Luc', 'Dubois', 'Ingénieur', '2011-10-19', '2011-10-19', '07 89 76 87 98', '03 45 21 24 56', 'dubois@gmail.fr');
 
 -- --------------------------------------------------------
@@ -55,7 +54,7 @@ INSERT INTO `contact` (`idcontact`, `identreprise`, `prenomcontact`, `nomcontact
 --
 
 CREATE TABLE IF NOT EXISTS `entreprise` (
-  `identreprise` int(11) NOT NULL AUTO_INCREMENT,
+  `identreprise` int(11) NOT NULL,
   `nomentreprise` varchar(50) DEFAULT NULL,
   `adresseentreprise` varchar(200) DEFAULT NULL,
   `villeentreprise` varchar(50) DEFAULT NULL,
@@ -87,20 +86,20 @@ INSERT INTO `entreprise` (`identreprise`, `nomentreprise`, `adresseentreprise`, 
 --
 
 CREATE TABLE IF NOT EXISTS `promotion` (
-  `idpromotion` int(11) NOT NULL AUTO_INCREMENT,
+  `idpromotion` int(11) NOT NULL,
   `nompromotion` varchar(50) DEFAULT NULL,
   `accesentreprises` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`idpromotion`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `promotion`
 --
 
 INSERT INTO `promotion` (`idpromotion`, `nompromotion`, `accesentreprises`) VALUES
-(1, 'L3', 0),
-(2, 'M2_SID', 1),
-(3, 'M2_ACSI', 1);
+(1, 'L3 2011-2012', 0),
+(2, 'M2 SID 2011-2012', 1),
+(3, 'M2 ACSI 2011-2012', 1);
 
 -- --------------------------------------------------------
 
@@ -109,7 +108,7 @@ INSERT INTO `promotion` (`idpromotion`, `nompromotion`, `accesentreprises`) VALU
 --
 
 CREATE TABLE IF NOT EXISTS `proposition` (
-  `idproposition` int(11) NOT NULL,
+  `idproposition` int(11) NOT NULL AUTO_INCREMENT,
   `identreprise` int(11) DEFAULT NULL,
   `idutilisateur` int(11) NOT NULL,
   `idstage` int(11) DEFAULT NULL,
@@ -122,20 +121,17 @@ CREATE TABLE IF NOT EXISTS `proposition` (
   `numerotelephonep` varchar(50) DEFAULT NULL,
   `urlsiteinternetp` varchar(50) DEFAULT NULL,
   `sujetstagep` text,
-  `estvalidee` tinyint(1) DEFAULT NULL,
+  `etat` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`idproposition`),
   KEY `fk_propoentreprise` (`identreprise`),
   KEY `fk_propositionstage2` (`idstage`),
   KEY `fk_utilisateur_proposition` (`idutilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `proposition`
 --
 
-INSERT INTO `proposition` (`idproposition`, `identreprise`, `idutilisateur`, `idstage`, `nomentreprisep`, `dateproposition`, `adresseentreprisep`, `villeentreprisep`, `codepostalentreprisep`, `paysentreprisep`, `numerotelephonep`, `urlsiteinternetp`, `sujetstagep`, `estvalidee`) VALUES
-(0, NULL, 1, NULL, 'Logica', '2011-10-26', '21 rue des pavé d''or', 'Luxembourg', '1021', 'Luxembourg', '00352303040506', 'http://www.logica.com', 'La chasse de l''alouette ambrée au Mexique ne succite-elle pas l''inquiétude des Hongrois ?', 0),
-(1, NULL, 1, NULL, 'Sopra', '2011-10-24', '28 avenue Montaigne', 'Luxembourg', '1234', 'Luxembourg', '00352123456', 'http://www.sopra.lu', 'Garantir la survie des opposoms en Alabama du Sud', 0);
 
 -- --------------------------------------------------------
 
@@ -145,8 +141,9 @@ INSERT INTO `proposition` (`idproposition`, `identreprise`, `idutilisateur`, `id
 
 CREATE TABLE IF NOT EXISTS `stage` (
   `idstage` int(11) NOT NULL,
-  `identreprise` int(11) NOT NULL,
+  `identreprise` int(11) DEFAULT NULL,
   `idcontact` int(11) DEFAULT NULL,
+  `idpromotion` int(11) DEFAULT NULL,
   `idproposition` int(11) NOT NULL,
   `idutilisateur` int(11) NOT NULL,
   `sujetstage` text,
@@ -162,8 +159,8 @@ CREATE TABLE IF NOT EXISTS `stage` (
   `embauche` tinyint(1) DEFAULT NULL,
   `dateembauche` date DEFAULT NULL,
   `respcivil` tinyint(1) DEFAULT NULL,
-  `promotionstagiaire` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`idstage`),
+  KEY `fk_association_9` (`idpromotion`),
   KEY `fk_propositionstage` (`idproposition`),
   KEY `fk_stage_contact` (`idcontact`),
   KEY `fk_stage_entreprise` (`identreprise`),
@@ -182,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `stage` (
 --
 
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `idutilisateur` int(11) NOT NULL,
+  `idutilisateur` int(11) NOT NULL AUTO_INCREMENT,
   `idpromotion` int(11) NOT NULL,
   `mailutilisateur` varchar(50) DEFAULT NULL,
   `passwordutilisateur` varchar(50) DEFAULT NULL,
@@ -192,15 +189,16 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `admin` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`idutilisateur`),
   KEY `fk_utilisateurpromotion` (`idpromotion`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Contenu de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`idutilisateur`, `idpromotion`, `mailutilisateur`, `passwordutilisateur`, `nomutilisateur`, `prenomutilisateur`, `numetudiant`, `admin`) VALUES
-(0, 2, 'khalid.benali@loria.fr', '94ca247fff5ad413788a1c8d8c80394a246dba1c', 'benali', 'khalid', NULL, 1),
-(1, 2, 'ludo@gmail.com', 'ae5a3c4fa3c5d1c2cc98e43b1899f88bce0e3569', 'fort', 'ludovic', '1234567', 0);
+(1, 2, 'ludo@gmail.com', 'ae5a3c4fa3c5d1c2cc98e43b1899f88bce0e3569', 'fort', 'ludovic', '1234567', 0),
+(3, 1, 'laurent@gmail.com', 'f1b010126f61b5c59e7d5eb42c5c68f6105c5914', 'Dubois', 'Laurent', '283983', 0),
+(10, 2, 'khalid.benali@loria.fr', '94ca247fff5ad413788a1c8d8c80394a246dba1c', 'benali', 'khalid', NULL, 1);
 
 --
 -- Contraintes pour les tables exportées
@@ -224,10 +222,11 @@ ALTER TABLE `proposition`
 -- Contraintes pour la table `stage`
 --
 ALTER TABLE `stage`
+  ADD CONSTRAINT `fk_utilistateur_stage` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateur` (`idutilisateur`),
+  ADD CONSTRAINT `fk_association_9` FOREIGN KEY (`idpromotion`) REFERENCES `promotion` (`idpromotion`),
   ADD CONSTRAINT `fk_propositionstage` FOREIGN KEY (`idproposition`) REFERENCES `proposition` (`idproposition`),
   ADD CONSTRAINT `fk_stage_contact` FOREIGN KEY (`idcontact`) REFERENCES `contact` (`idcontact`),
-  ADD CONSTRAINT `fk_stage_entreprise` FOREIGN KEY (`identreprise`) REFERENCES `entreprise` (`identreprise`),
-  ADD CONSTRAINT `fk_utilistateur_stage` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateur` (`idutilisateur`);
+  ADD CONSTRAINT `fk_stage_entreprise` FOREIGN KEY (`identreprise`) REFERENCES `entreprise` (`identreprise`);
 
 --
 -- Contraintes pour la table `utilisateur`
